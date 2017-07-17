6ce881d5d6258a2ada6ab43c9b80f75b127791d4
youngjung <crazyyfish@naver.com>
2017-07-17 17:40:31 +0900

    train_op for adversarial learning seems working

--- im2txt/train.py
+++ im2txt/train.py
@@ -21,6 +21,7 @@ from __future__ import print_function
 import math
 import os
 import time
+import numpy as np
 import tensorflow as tf
 
 import configuration
@@ -75,8 +76,8 @@ def main(unused_argv):
 
 		# Build the model (free-running mode).
 		model_free = show_and_tell_model.ShowAndTellModel(
-				model_config, mode="free", vocab=vocab, reuse=True )
-		model_free.build()
+				model_config, mode="free", train_inception=FLAGS.train_inception, vocab=vocab, reuse=True )
+		model_free.build([model.images,model.input_seqs,model.target_seqs,model.input_mask])
 
 		# Build the model for validation with variable sharing
 		model_valid = show_and_tell_model.ShowAndTellModel(
@@ -104,15 +105,23 @@ def main(unused_argv):
 							output_keep_prob=model_config.lstm_dropout_keep_prob)
 
 		with tf.variable_scope("discriminator") as scope_disc:
-			teacher_lengths = tf.reduce_sum( model.input_mask, 1 )
+			teacher_lengths = tf.reduce_sum( model.input_mask, 1 )+2
+			free_lengths = tf.ones_like(teacher_lengths)*(30+2)
+
+			# run lstm
 			d_outputs_teacher, _ = tf.nn.dynamic_rnn( cell=d_lstm_cell,
 												inputs = teacher_behavior,
 												sequence_length = teacher_lengths,
 												dtype = tf.float32,
 												scope = scope_disc )
-			#d_outputs_teacher = tf.transpose( d_outputs_teacher, [1,0,2] )
-			#d_last_output_teacher = tf.gather( d_outputs_teacher, int(d_outputs_teacher.get_shape()[0])-1 )
-			d_last_output_teacher  = tf.slice( d_outputs_teacher, [0,-1,0],[-1,-1,-1] )
+
+			# gather last outputs (deals with variable length of captions)
+			teacher_lengths = tf.expand_dims( teacher_lengths, 1 )
+			batch_range = tf.expand_dims(tf.constant( np.array(range(model_config.batch_size)),dtype=tf.int32 ),1)
+			gather_idx = tf.concat( [batch_range,teacher_lengths-1], axis=1 )
+			d_last_output_teacher = tf.gather_nd( d_outputs_teacher, gather_idx )
+
+			# FC to get T/F logits
 			d_logits_teacher = tf.contrib.layers.fully_connected( inputs = d_last_output_teacher,
 															num_outputs = 2,
 															activation_fn = None,
@@ -120,29 +129,27 @@ def main(unused_argv):
 															scope = scope_disc )
 
 			scope_disc.reuse_variables()
-			free_lengths = tf.ones_like(teacher_lengths)*30
 			d_outputs_free, _ = tf.nn.dynamic_rnn( cell=d_lstm_cell,
 												inputs = free_behavior,
 												sequence_length = free_lengths,
 												dtype = tf.float32,
 												scope = scope_disc )
-			#d_outputs_free = tf.transpose( d_outputs_free, [1,0,2] )
-			#d_last_output_free = tf.gather( d_outputs_free, int(d_outputs_free.get_shape()[0])-1 )
-			d_last_output_free = tf.slice( d_outputs_free, [0,-1,0],[-1,-1,-1] )
+			d_last_output_free = d_outputs_free[:,-1,:]
 			d_logits_free = tf.contrib.layers.fully_connected( inputs = d_last_output_free,
 															num_outputs = 2,
 															activation_fn = None,
 															weights_initializer = model.initializer,
 															scope = scope_disc )
 
-		d_loss_teacher = tf.reduce_mean( tf.nn.sigmoid_cross_entropy_with_logits(
+		d_loss_teacher = tf.reduce_mean( tf.nn.sigmoid_cross_entropy_with_logits(name='d_loss_teacher',
 									 logits=d_logits_teacher, labels=tf.ones_like(d_logits_teacher) ) )
-		d_loss_free = tf.reduce_mean( tf.nn.sigmoid_cross_entropy_with_logits(
+		d_loss_free = tf.reduce_mean( tf.nn.sigmoid_cross_entropy_with_logits(name='d_loss_free',
 									 logits=d_logits_free, labels=tf.zeros_like(d_logits_free) ) )
 		d_loss = d_loss_teacher + d_loss_free
 
-		g_loss = tf.reduce_mean( tf.nn.sigmoid_cross_entropy_with_logits(
+		g_loss = tf.reduce_mean( tf.nn.sigmoid_cross_entropy_with_logits(name='g_loss',
 									 logits=d_logits_free, labels=tf.ones_like(d_logits_free) ) )
+		g_and_NLL_loss = g_loss + model.total_loss
 
 		summary = {}
 		summary['NLL_loss'] = tf.summary.scalar('NLL_loss', model.total_loss)
@@ -150,6 +157,8 @@ def main(unused_argv):
 		summary['d_loss_teacher'] = tf.summary.scalar('d_loss_teacher', d_loss_teacher)
 		summary['d_loss_free'] = tf.summary.scalar('d_loss_free', d_loss_free)
 		summary['g_loss'] = tf.summary.scalar('g_loss', g_loss)
+		summary['g_and_NLL_loss'] = tf.summary.scalar('g_and_NLL_loss', g_and_NLL_loss)
+		summary['d_logits_free'] = tf.summary.histogram('d_logits_free', d_logits_free)
 
 		# Set up the learning rate for training ops
 		learning_rate_decay_fn = None
@@ -157,6 +166,7 @@ def main(unused_argv):
 			learning_rate = tf.constant(training_config.train_inception_learning_rate)
 		else:
 			learning_rate = tf.constant(training_config.initial_learning_rate)
+			learning_rate1 = tf.constant(training_config.initial_learning_rate)
 			if training_config.learning_rate_decay_factor > 0:
 				num_batches_per_epoch = (training_config.num_examples_per_epoch //
 																 model_config.batch_size)
@@ -172,6 +182,7 @@ def main(unused_argv):
 							staircase=True)
 
 				learning_rate_decay_fn = _learning_rate_decay_fn
+				learning_rate_decay_fn1 = _learning_rate_decay_fn
 
 		# Collect trainable variables
 		vars_all = [ v for v in tf.trainable_variables() if v not in model.inception_variables ]
@@ -186,16 +197,18 @@ def main(unused_argv):
 											optimizer = training_config.optimizer,
 											clip_gradients = training_config.clip_gradients,
 											learning_rate_decay_fn = learning_rate_decay_fn,
-											variables = g_vars )
+											variables = g_vars,
+											name='optimize_NLL_loss' )
 
 		train_op_disc = tf.contrib.layers.optimize_loss(
 											loss = d_loss,
 											global_step = model.global_step,
-											learning_rate = learning_rate,
+											learning_rate = learning_rate1,
 											optimizer = training_config.optimizer,
 											clip_gradients = training_config.clip_gradients,
-											learning_rate_decay_fn = learning_rate_decay_fn,
-											variables = d_vars )
+											learning_rate_decay_fn = learning_rate_decay_fn1,
+											variables = d_vars,
+											name='optimize_disc_loss' )
 
 		train_op_gen = tf.contrib.layers.optimize_loss(
 											loss=model.total_loss+g_loss,
@@ -204,7 +217,8 @@ def main(unused_argv):
 											optimizer=training_config.optimizer,
 											clip_gradients=training_config.clip_gradients,
 											learning_rate_decay_fn=learning_rate_decay_fn,
-											variables = g_vars)
+											variables = g_vars,
+											name='optimize_gen_loss' )
 
 
 
@@ -230,14 +244,13 @@ def main(unused_argv):
 			could_load, checkpoint_counter = load( sess, saver, train_dir )
 			if could_load:
 				counter = checkpoint_counter
-			lossnames_to_print = ['g_loss']
 			generator = caption_generator.CaptionGenerator( model_valid, vocab )
 
 			try:
 				# for validation
 				with tf.gfile.GFile('data/mscoco/raw-data/val2014/COCO_val2014_000000224477.jpg','r') as f:
 					image_valid = f.read()
-				f_valid_text = open('valid.txt','w')
+				f_valid_text = open('valid.txt','a')
 			
 				# run inference for not-trained model
 				#self.valid( valid_image, f_valid_text )
@@ -253,15 +266,38 @@ def main(unused_argv):
 
 
 				# run training loop
+				lossnames_to_print = ['NLL_loss','g_loss', 'd_loss']
+				NLL_loss_val = float('Inf')
+				g_loss_val = float('Inf')
+				d_loss_val = float('Inf')
 				for epoch in range(FLAGS.number_of_steps):
 					for batch_idx in range(nBatches):
 						counter += 1
-						_, g_loss, summary_str = sess.run([train_op_NLL, model.total_loss, summary['NLL_loss']] )
-						summaryWriter.add_summary(summary_str, counter)
+						if NLL_loss_val > 3.5:
+							_, NLL_loss_val, summary_str = sess.run([train_op_NLL, model.total_loss, summary['NLL_loss']] )
+							summaryWriter.add_summary(summary_str, counter)
+						else:
+							#val_outputs_teacher, val_outputs_free, val_teacher_length, val_gather_idx,\
+							#	val_last_output_teacher, val_last_output_free = sess.run(
+							#	[d_outputs_teacher, d_outputs_free,teacher_lengths,gather_idx,\
+							#	d_last_output_teacher, d_last_output_free])
+							#pdb.set_trace()
+							_, d_loss_val, smr1, smr2, smr3, smr4 = sess.run([train_op_disc, d_loss,
+								 summary['d_loss_teacher'], summary['d_loss_free'], summary['d_loss'],summary['d_logits_free']] )
+							summaryWriter.add_summary(smr1, counter)
+							summaryWriter.add_summary(smr2, counter)
+							summaryWriter.add_summary(smr3, counter)
+							summaryWriter.add_summary(smr4, counter)
+							_, g_loss_val, smr1, smr2, smr3 = sess.run( [train_op_gen,g_loss
+								,summary['g_loss'],summary['NLL_loss'], summary['g_and_NLL_loss']] )
+							summaryWriter.add_summary(smr1, counter)
+							summaryWriter.add_summary(smr2, counter)
+							
 			
 						if counter % FLAGS.log_every_n_steps==0:
 							elapsed = time.time() - start_time
-							log( epoch, batch_idx, nBatches, lossnames_to_print, [g_loss], elapsed, counter )
+							log( epoch, batch_idx, nBatches, lossnames_to_print,
+								 [NLL_loss_val,g_loss_val,d_loss_val], elapsed, counter )
 			
 						if counter % 500 == 1 or \
 							(epoch==FLAGS.number_of_steps-1 and batch_idx==nBatches-1) :
@@ -271,7 +307,7 @@ def main(unused_argv):
 							# run test after every epoch
 							#self.valid( valid_image, f_valid_text )
 							captions = generator.beam_search( sess, image_valid )
-							f_valid_text.write( 'epoch {} batch {}/{}\n'.format( epoch, batch_idx ) )
+							f_valid_text.write( 'epoch {} batch {}/{}\n'.format( epoch, batch_idx, nBatches ) )
 							for i, caption in enumerate(captions):
 								sentence = [vocab.id_to_word(w) for w in caption.sentence[1:-1]]
 								sentence = " ".join(sentence)
@@ -316,15 +352,15 @@ def log( epoch, batch, nBatches, lossnames, losses, elapsed, counter=None, filel
 		if type(loss) == str:
 			str_losses += loss
 		else:
-			str_losses += '{:.2f}'.format(loss)
+			str_losses += '{:.4f}'.format(loss)
 		isFirst = False
 
 	m,s = divmod( elapsed, 60 )
 	h,m = divmod( m,60 )
 	timestamp = "{:2}:{:02}:{:02}".format( int(h),int(m),int(s) )
-	log = "{} epoch {} batch {:>{}}/{} ({})=({})".format( timestamp, epoch, batch, nDigits, nBatches, str_lossnames, str_losses )
+	log = "{} e{} b {:>{}}/{} ({})=({})".format( timestamp, epoch, batch, nDigits, nBatches, str_lossnames, str_losses )
 	if counter is not None:
-		log = "cnt{:>5} ".format(counter) + log
+		log = "{:>5}_".format(counter) + log
 	print( log )
 	if filelogger:
 		filelogger.write( log )
