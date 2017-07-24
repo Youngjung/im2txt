921c668e065313184b420c814a71029e28c3403d
youngjung <crazyyfish@naver.com>
2017-07-24 17:26:56 +0900

    fresh start from official tensorflow im2txt

---
+++ train.sh
@@ -0,0 +1,21 @@
+# Inception v3 checkpoint file.
+IM2TXT="${HOME}/youngjung/im2txt"
+INCEPTION_CHECKPOINT="${IM2TXT}/model/inception_v3.ckpt"
+
+# Directory to save the model.
+MODEL_DIR="${IM2TXT}/model/train"
+
+# Build the model.
+#bazel build -c opt im2txt/...
+
+export CUDA_VISIBLE_DEVICES=0
+# Run the training script.
+#bazel-bin/im2txt/train \
+python im2txt/train.py \
+  --input_file_pattern="${MSCOCO_DIR}/forShowAndTell/train-?????-of-00256" \
+  --inception_checkpoint_file="${INCEPTION_CHECKPOINT}" \
+  --train_dir="${MODEL_DIR}" \
+  --train_inception=false \
+  --number_of_steps=1000000 \
+  --log_every_n_steps=10 \
+  --vocab_file="${MSCOCO_DIR}/forShowAndTell/word_counts.txt"
